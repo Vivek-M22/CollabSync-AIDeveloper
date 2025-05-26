dfb96f7d7703f05a75f0fc961bd33e39997e2800
Vivek Makwana <vivekmakwana.one@gmail.com>
2025-05-26 16:31:05 +0530

    adding all frontend and backend files

---
+++ frontend/src/context/user.context.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react-refresh/only-export-components */
+import { createContext, useState, } from 'react';
+
+// Create the UserContext
+export const UserContext = createContext();
+
+// Create a provider component
+export const UserProvider = ({ children }) => {
+    const [ user, setUser ] = useState(null);
+
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            {children}
+        </UserContext.Provider>
+    );
+};
+
+
